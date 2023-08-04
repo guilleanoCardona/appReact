d9d2960615eae5fc693f2bcf18fabaa58a5788e8
Guilleano Cardona <gcardona@tecnoglass.com>
2023-08-04 17:04:08 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import getGifs from "./services/getGifs";
+import ListOfGifs from "./components/ListOfGifs";
 
-function App() {
-  const [gifs, setGifs] = useState([]);
-
-  useEffect(function () {
-    getGifs({ keyword: "rick" }).then((gifs) => setGifs(gifs));
-  }, []);
+export default function App() {
   return (
     <div className="App">
       <section className="App-content">
-        {gifs.map((singleGif) => (
-          <img src={singleGif} />
-        ))}
-        {/* <button onClick={() => setGifs(DIERENTS_GIFS)}>Cambiar Gifs</button> */}
+        <ListOfGifs keyword="panda" />
       </section>
     </div>
   );
 }
-
-export default App;
--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -2,13 +2,17 @@ const apiKey = "zS4q6JOkA2Gemp06VzSDhOquvdh052o2";
 
 export default function getGifs({ keyword = "morty" } = {}) {
   const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
   return fetch(apiUrl)
     .then((res) => res.json())
     .then((response) => {
-      const { data } = response;
-      const gifs = data.map(
-        (image) => image.images.fixed_height_downsampled.url
-      );
+      const { data = [] } = response;
+      const gifs = data.map((image) => {
+        const { images, title, id } = image;
+        const { url } = images.fixed_height_downsampled;
+
+        return { title, id, url };
+      });
       return gifs;
     });
 }
